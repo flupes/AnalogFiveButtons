b9a1c28dd9b5f0845dadb32fa9d40529352e6e37
Lorenzo Flueckiger <lorenzo@flupes.org>
2011-04-20 23:00:01 -0700

    Added the class files

---
+++ AnalogFiveButtons.cpp
@@ -0,0 +1,170 @@
+#include <WProgram.h>
+
+#include "AnalogFiveButtons.h"
+
+#include "wiring.h"
+
+AnalogFiveButtons::AnalogFiveButtons(int analogPin, float defaultAnalogRef) :
+  m_analogPin(analogPin), m_defaultAnalogRef(defaultAnalogRef),
+  m_currentStateIndex(0)
+{
+  m_refVoltage = 5.0;
+
+  m_resistors[0] = 4700;
+  m_resistors[1] = 22100;
+  m_resistors[2] = 10000;
+  m_resistors[3] = 4700;
+  m_resistors[4] = 2100;
+  m_resistors[5] = 1200;
+
+  m_msSampling = 50;
+  m_debounceCount = 2;
+  
+  m_states[0] = 0;	// No button pressed
+  m_states[1] = 1;	// B1 pressed
+  m_states[2] = 2;	// B2 pressed
+  m_states[3] = 3;	// B1 + B2 pressed
+  m_states[4] = 4;	// B3 pressed
+  m_states[5] = 5;	// B1 + B3 pressed
+  m_states[6] = 6;	// B2 + B3 pressed
+  m_states[7] = 8;	// B4 pressed
+  m_states[8] = 9;	// B1 + B4 pressed
+  m_states[9] = 10;	// B2 + B4 pressed
+  m_states[10] = 12;	// B3 + B4 pressed
+  m_states[11] = 16;	// B5 pressed
+  m_states[12] = 17;	// B1 + B5 pressed
+  m_states[13] = 18;	// B2 + B5 pressed
+  m_states[14] = 20;	// B3 + B5 pressed
+  m_states[15] = 24;	// B4 + B5 pressed
+
+  computeLadder();
+}
+
+void AnalogFiveButtons::setTiming(int msSampling, byte debounceCount)
+{
+  m_msSampling = msSampling;
+  m_debounceCount = debounceCount;
+}
+
+void AnalogFiveButtons::setLadder(float refVoltage, word R[6])
+{
+  m_refVoltage = refVoltage;
+  for (byte i=0; i<6; i++) {
+    m_resistors[i] = R[i];
+  }
+  computeLadder();
+}
+
+boolean AnalogFiveButtons::removeState(byte state)
+{
+  for (byte i=0; i<16; i++) {
+    if ( m_states[i] == state ) {
+      m_ladder[i] = 2048;
+      return true;
+    }
+  }
+  return false;
+}
+
+void AnalogFiveButtons::computeLadder()
+{
+  // we have:
+  // Req = 1 / ( B1/R1 + B2/R2 + B3/R3 + B4/R4 + B5/R5)
+  // notation: Bn/Rn means if Bn down, then 1/Rn, otherwise 0
+  // Vout = refVoltage * Req / ( Req + R0 )
+
+  float Req;
+  float Vout;
+  m_ladder[0] = (int)( 1024.0*m_refVoltage/m_defaultAnalogRef );
+  for (byte i=1; i<16; i++) {
+    Req = 1.0 / (
+		 ( BM_1 & m_states[i] ? 1.0/(float)m_resistors[1] : 0.0 ) +
+		 ( BM_2 & m_states[i] ? 1.0/(float)m_resistors[2] : 0.0 ) +
+		 ( BM_3 & m_states[i] ? 1.0/(float)m_resistors[3] : 0.0 ) +
+		 ( BM_4 & m_states[i] ? 1.0/(float)m_resistors[4] : 0.0 ) +
+		 ( BM_5 & m_states[i] ? 1.0/(float)m_resistors[5] : 0.0 )
+		 );
+    Vout = m_refVoltage*Req/(Req+(float)m_resistors[0]);
+    m_ladder[i] = (int)( 1024.0*Vout/m_defaultAnalogRef );
+  }
+
+}
+
+byte AnalogFiveButtons::computeState(int analogReading)
+{
+  int error;
+  byte index = 0;
+  int minError = 1024;
+#ifdef A5B_DEBUG
+  Serial.print("==== New reading: ");
+  Serial.println(analogReading, DEC);
+#endif
+
+  for (byte i=0; i<16; i++) {
+    error = abs( m_ladder[i] - analogReading );
+#ifdef A5B_DEBUG
+    Serial.print(i, DEC);
+    Serial.print(" : ");
+    Serial.print(m_ladder[i], DEC);
+    Serial.print(" -> ");
+    Serial.println(error, DEC);
+#endif
+    if ( error < minError ) {
+      minError = error;
+      index = i;
+    }
+  }
+
+#ifdef A5B_DEBUG
+  Serial.print("  -> index=");
+  Serial.print(index);
+  Serial.print(" => ");
+  Serial.print(m_states[index], DEC);
+  Serial.print(" = ");
+  Serial.println(m_states[index], BIN);
+#endif
+
+  return index;
+}
+
+void AnalogFiveButtons::update()
+{
+  static int previousReading = 0;
+  static unsigned int previousSampleTime = 0;
+  static byte counter = 0;
+
+  // Get current time
+  unsigned long currentSampleTime = millis();
+
+  // Start to process only if enough time has elapsed
+  if ( currentSampleTime > (previousSampleTime+m_msSampling) ) {
+
+    // Read the current analog input pin
+    int currentReading = analogRead(m_analogPin);
+
+    // Check if the reading is not varying much from the previous
+    if ( abs(previousReading-currentReading) < 3 ) {
+
+      counter++;	// new stable reading
+      if ( counter == m_debounceCount ) {
+	// compute the new state
+	m_currentStateIndex = computeState(currentReading);
+	counter = 0;
+      }
+
+    }
+    previousReading = currentReading;
+    previousSampleTime = currentSampleTime;
+
+  }
+}
+
+boolean AnalogFiveButtons::getState(byte button)
+{
+  return ( button & m_states[m_currentStateIndex] );
+}
+
+byte AnalogFiveButtons::getState()
+{
+  return m_states[m_currentStateIndex];
+}
---
+++ AnalogFiveButtons.h
@@ -0,0 +1,130 @@
+//#include <stdint.h>
+
+#include <wiring.h>	// for types "byte" and "boolean"
+			// wiring.h is required anyway in the 
+			// implementation for analogRead
+
+/** AnalogFiveButtons provides the abstraction layer for a five button
+    input device.
+    
+    AnalogFiveButtons use a *single* analog input to detect the state
+    of 5 switches. This is done using an resitor ladder network: each
+    closed circuit will change the equivalent resistance of the lower
+    portion of the resistor divider.
+
+    The class debounces the signal by comparing two successives
+    readings. Finally, the class allow to detect when 2 buttons are
+    pressed simultaneously.
+
+    The hardware circuit should be constructed as follow:
+
+    refVoltage -+
+                |
+	        R0
+	        |
+    analogIn ---+-------+-------+-------+-------+
+		|       |       |       |       |
+                R1      R2      R3      R4      R5
+		|       |       |       |       |
+               |: B1   |: B2   |: B3   |: B4   |: B5
+		|       |       |       |       |
+    GND --------+-------+-------+-------+-------+
+
+    R0 ~= R3
+    R5 < R4 < R3 < R2 < R1
+
+    This class could be used with less than five buttons, the unused
+    slots will just never be active and state for these buttons will
+    always be up.
+
+    This class could be generalized for a arbitrary number of
+    buttons. However, for 3 buttons systems, the gain of the unique
+    analog becomes only insignificant. For more than 6 buttons, the
+    variation in voltage created by the resitor ladder will be very
+    small and may lead to wrong readings. Multiplexers or other
+    systems will be better suited for input systems requiring more
+    than 5 buttons.
+  */
+class AnalogFiveButtons {
+  
+ public:
+
+  /** Constructor.
+      
+      @param analogPin		which pin should be used to read the
+				analog signal
+      @param defaultAnalogRef	the default analog reference used by your
+				Arduino board (5V or 3.3V)
+
+      @note AnalogFiveButtons construct a default resistor ladder. If you 
+      are using different resistors or a different reference voltage, you 
+      need to use setLadder before using the object.
+  */
+  AnalogFiveButtons(int analogPin, float defaultAnalogRef);
+  
+  /** Define the resitor ladder.
+      
+      @param refVoltagereference	voltage at the top of the ladder
+      @param R[]			list of resitor values.
+      The resistance are expressed in Omhs. The first element of the
+      array (R[0]) is the pull up. R[1] to R[5] are the resitance for the
+      switches 1 to 5. Note that because of the coding resitance more
+      than 65K cannot be used...
+  */
+  void setLadder(float refVoltage, word R[6]);
+
+
+  void setTiming(int msSampling, byte debounceCount);
+
+  boolean removeState(byte state);
+
+  /** Returns if the given button is pressed or not.
+      @param button	the button code. It is necessary to used the
+			defined constants B1 - B5.
+      @return		1 if the button is pressed, 0 otherwise
+   */
+  boolean getState(byte button);
+
+  const static byte BM_1 = 1;	/** mask defining button 1 */
+  const static byte BM_2 = 2;	/** mask defining button 2 */
+  const static byte BM_3 = 4;	/** mask defining button 3 */
+  const static byte BM_4 = 8;	/** mask defining button 4 */
+  const static byte BM_5 = 16;	/** mask defining button 5 */
+
+  /** Returns the states of all buttons coded on a a char.
+   */
+  byte getState();
+
+  /** Update the current button state by reading the analog pin
+      and computing the resulting state.
+   */
+  void update();
+
+  int analogPin() { return m_analogPin; }
+
+ protected:
+  /** Compute the voltage output for each state of the ladder, coded
+      on 10 bits.
+  */
+  void computeLadder();
+
+  /** Returns the state associated with the given reading of the input
+      voltage.
+   */
+  byte computeState(int analogReading);
+
+  int m_analogPin;		/** pin used to read the analog input */
+  float m_defaultAnalogRef;	/** default analog reference for the board */
+  int m_msSampling;		/** Required delay between 2 successive
+			readings (in ms) */
+  byte m_debounceCount;		/** Number of succesives readings need to 
+			be identical before we decide the voltage is stable */
+
+  float m_refVoltage;		/** reference voltage at the top of the ladder */
+  word m_resistors[6];
+  byte m_states[16];
+  int m_ladder[16];
+  byte m_currentStateIndex;
+  
+};
+
