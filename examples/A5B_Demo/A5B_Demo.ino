a6846b2b24e0bc92770092b1f035dbf2695c43a7
lfluckig <lorenzo@flupes.org>
2015-02-01 18:21:49 -0800

    better test example

--- examples/A5B_Demo/A5B_Demo.ino
+++ examples/A5B_Demo/A5B_Demo.ino
@@ -2,7 +2,8 @@
 
 #include "AnalogFiveButtons.h"
 
-#define WAIT_LOOP 200
+#define LOOP_PERIOD 25
+#define PRINT_PERIOD 500
 
 AnalogFiveButtons a5b(A0, 3.3);
 word ladder[6] = { 4990, 22100, 9310, 4990, 2100, 909 };
@@ -20,7 +21,7 @@ void setup() {
   a5b.setLadder(3.3, ladder);
   a5b.removeState(17);
   a5b.removeState(18);
-  a5b.setTiming(200, 2);
+  a5b.setTiming(50, 2);
 
   counter = 0;
   prevtime = millis();
@@ -30,19 +31,29 @@ void loop()
 {
   a5b.update();
 
-  byte bm = 1;
-  for ( int i=0; i<5; i++ ) {
-    Serial.print(" | ");
-    Serial.print(a5b.getState(bm), DEC);
-    bm = bm << 1;
-  }
-  Serial.println(" |");
+  // byte bm = 1;
+  // for ( int i=0; i<5; i++ ) {
+  //   Serial.print(" | ");
+  //   Serial.print(a5b.getState(bm), DEC);
+  //   bm = bm << 1;
+  // }
+  // Serial.println(" |");
   
   unsigned long newtime = millis();
   int elapsed = newtime-prevtime;
+  if ( elapsed > PRINT_PERIOD ) {
+    state = a5b.getPressedState();
+    Serial.println(state, BIN);
+    if ( state > 0 ) {
+      counter++;
+    }
+    if ( counter == 6 ) {
+      a5b.clearButton(AnalogFiveButtons::BM_3);
+      counter = 0;
+    }
+    prevtime = newtime;
+  }
 
-  counter++;
-
-  delay(WAIT_LOOP);
+  delay(LOOP_PERIOD);
 
 }
